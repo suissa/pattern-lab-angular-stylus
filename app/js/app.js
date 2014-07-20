99f6f23c2541de64f24e7a626102c4b062074690
Suissa <jnascimento@gmail.com>
2014-07-19 22:40:01 -0300

    Criação das rotas com suas views

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('PatternLab', [
   'ngRoute',
   'myApp.filters',
   'myApp.services',
@@ -10,14 +10,12 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/pattern-lab', {templateUrl: 'views/index.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/settings', {templateUrl: 'views/settings.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/atoms', {templateUrl: 'views/atoms.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/molecules', {templateUrl: 'views/molecules.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/organisms', {templateUrl: 'views/organisms.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/templates', {templateUrl: 'views/templates.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/pages', {templateUrl: 'views/pages.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/pattern-lab', {templateUrl: 'views/index.html');
+  $routeProvider.when('/settings', {templateUrl: 'views/settings.html');
+  $routeProvider.when('/atoms', {templateUrl: 'views/atoms.html');
+  $routeProvider.when('/molecules', {templateUrl: 'views/molecules.html');
+  $routeProvider.when('/organisms', {templateUrl: 'views/organisms.html');
+  $routeProvider.when('/templates', {templateUrl: 'views/templates.html');
+  $routeProvider.when('/pages', {templateUrl: 'views/pages.html');
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
