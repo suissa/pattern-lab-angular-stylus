bf01b48dc53b87496944e039f65c999e31ca8005
Suissa <jnascimento@gmail.com>
2014-07-20 11:04:55 -0300

    loadCss e loadScript em Service agora

--- app/js/atoms.js
+++ app/js/atoms.js
@@ -3,30 +3,70 @@
 /* Atoms */
 
 angular.module('myApp.atoms', []).
-  directive('atomInputText', [function () {
+  directive('atomInputText', ['$document', '$q', '$timeout', 'AngularLoad', function ($document, $q, $timeout, AngularLoad) {
+    var name = 'atom-input-text';
     return {
-      restrict: 'AE',
-      templateUrl: 'views/atoms/atom-input-text.html',
-      link: function (scope, iElement, iAttrs) {
-        
-      }
-    };
-  }]).
-  directive('atomInputLogin', [function () {
-    return {
-      restrict: 'AE',
-      templateUrl: 'views/atoms/atom-input-login.html',
-      link: function (scope, iElement, iAttrs) {
-        
-      }
-    };
-  }]).
-  directive('atomButton', [function () {
-    return {
-      restrict: 'AE',
-      templateUrl: 'views/atoms/atom-button.html',
-      link: function (scope, iElement, iAttrs) {
-        
+      restrict: 'E',
+      templateUrl: 'views/atoms/'+name+'.html?'+Date.now(),
+      // template: '<input type="text" class="atom-input-text">',
+      link: function (scope, elm, attrs, ctrl) {
+        var path = 'css/atoms/'+name+'.css';
+        AngularLoad.loadCSS(path, $document, $q, $timeout);
       }
     };
   }]);
+  // directive('atomInputText', ['$document', '$q', '$timeout', function ($document, $q, $timeout) {
+  //   var name = 'atom-input-text';
+  //   return {
+  //     restrict: 'AE',
+  //     templateUrl: 'views/atoms/'+name+'.html',
+  //     link: function (scope, iElement, iAttrs) {
+  //       var path = 'css/atoms/'+name+'.css';
+  //       loadCSS(path, $document, $q, $timeout);
+  //     }
+  //   };
+  // }]).
+  // directive('atomInputLogin', ['$document', '$q', '$timeout', function ($document, $q, $timeout) {
+  //   var name = 'atom-input-login';
+  //   return {
+  //     restrict: 'AE',
+  //     templateUrl: 'views/atoms/'+name+'.html',
+  //     link: function (scope, iElement, iAttrs) {
+  //       var path = 'css/atoms/'+name+'.css';
+  //       // loadCSS(path, $document, $q, $timeout);
+  //     }
+  //   };
+  // }]).
+  // directive('atomInputCpf', ['$document', '$q', '$timeout', function ($document, $q, $timeout) {
+  //   var name = 'atom-input-cpf';
+  //   return {
+  //     restrict: 'AE',
+  //     tranclude: true,
+  //     scope: {
+  //       ngModel: '=',
+  //     },
+  //     templateUrl: 'views/atoms/'+name+'.html',
+  //     // template: 'POOOORRRAA',
+  //     link: function (scope, elm, attrs, ctrl)  {
+  //       console.log(attrs);
+  //       var path = 'css/atoms/'+name+'.css';
+  //       // loadCSS(path, $document, $q, $timeout);
+  //       var pattern = /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/;
+  //       scope.$watch('ngModel', function(newVal, oldVal) {
+  //         console.log('cpf: ', newVal);
+  //       });
+
+  //     }
+  //   };
+  // }]).
+  // directive('atomButton', ['$document', '$q', '$timeout', function ($document, $q, $timeout) {
+  //   var name = 'atom-button';
+  //   return {
+  //     restrict: 'AE',
+  //     templateUrl: 'views/atoms/'+name+'.html',
+  //     link: function (scope, iElement, iAttrs) {
+  //       var path = 'css/atoms/'+name+'.css';
+  //       // loadCSS(path, $document, $q, $timeout);
+  //     }
+  //   };
+  // }]);
